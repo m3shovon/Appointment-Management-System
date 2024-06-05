4b4cba361385a10add815e748200ec2c0a255e47
Mufrid Shovon <mia.md.mufrid@gmail.com>
2024-06-05 17:56:42 +0600

    new

---
+++ eventcalendar/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from .views import DashboardView
+
+
+urlpatterns = [
+    path("", DashboardView.as_view(), name="dashboard"),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("", include("calendarapp.urls")),
+]
