12535128951ae49e7189537de9d19e4a6e520660
Mufrid Shovon <mia.md.mufrid@gmail.com>
2024-09-19 15:18:22 +0600

    Updated Calender

--- accounts/views/signin.py
+++ accounts/views/signin.py
@@ -1,29 +1,33 @@
 from django.views.generic import View
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 from accounts.forms import SignInForm
 
 
 class SignInView(View):
-    """ User registration view """
+    """ User sign-in view """
 
     template_name = "accounts/signin.html"
     form_class = SignInForm
 
     def get(self, request, *args, **kwargs):
-        forms = self.form_class()
-        context = {"form": forms}
+        form = self.form_class()
+        context = {"form": form}
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
-        forms = self.form_class(request.POST)
-        if forms.is_valid():
-            email = forms.cleaned_data["email"]
-            password = forms.cleaned_data["password"]
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
             user = authenticate(email=email, password=password)
             if user:
                 login(request, user)
-                return redirect("calendarapp:calendar")
-        context = {"form": forms}
+                messages.success(request, "Authentication successful! Redirecting to dashboard...")
+                return render(request, self.template_name, {"form": form})
+            else:
+                messages.error(request, "Authentication failed. Invalid email or password.")
+        context = {"form": form}
         return render(request, self.template_name, context)
