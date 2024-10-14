fef8d77e9582dd7e1b6fcd2eb58249bf52f601f2
Mufrid Shovon <mia.md.mufrid@gmail.com>
2024-10-14 10:39:04 +0600

    update

--- eventcalendar/urls.py
+++ eventcalendar/urls.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from schema_graph.views import Schema
 
 from .views import DashboardView
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("", include("calendarapp.urls")),
+    path("schema/", Schema.as_view()),
 ]
